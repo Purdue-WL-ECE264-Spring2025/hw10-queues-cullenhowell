144d7474a62dca5ae94657ed8f03b0928f1098d1
cullenhowell <cullenhowell03@gmail.com>
2025-03-31 23:58:23 -0400

    Linked list functions complete

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,75 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) 
+{
+  struct list_node * nd = malloc(sizeof(struct list_node));
+  if (nd == NULL) return NULL;
+  nd -> value = value;
+  nd -> next = NULL;
+  return nd;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) 
+{
+  struct list_node * ptr = new_node(value);
+  if (ptr == NULL) return;
+  ptr -> next = list -> head;
+  list -> head = ptr; 
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) 
+{
+  struct list_node * ptr = new_node(value);
+  if (ptr == NULL) return;
+  if (list -> head == NULL)
+  {
+    list -> head = ptr;
+    return;
+  }
+  struct list_node * current = list -> head;
+  while ((current -> next) != NULL) current = current -> next;
+  current -> next = ptr;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) 
+{
+  if (list -> head == NULL) return 0;
+  struct list_node * temp_head = list -> head;
+  size_t temp_val = temp_head -> value;
+  list -> head = temp_head -> next;
+  free(temp_head);
+  return temp_val;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) 
+{
+  if (list -> head == NULL) return 0;
+  if (list -> head -> next == NULL)
+  {
+    size_t temp_val = list -> head -> value;
+    free(list -> head);
+    list -> head = NULL;
+    return temp_val;
+  }
+  struct list_node * current = list -> head;
+  while ((current -> next -> next) != NULL) current = current -> next;
+  size_t temp_val = current -> next -> value;
+  current -> next = NULL;
+  return temp_val;
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) 
+{
+  struct list_node * current = list.head;
+  while (current != NULL)
+  {
+    struct list_node * next = current -> next;
+    free(current);
+    current = next;
+  }
+  list.head = NULL;
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,62 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+// enqueue function: Add a serialized game state to the queue
+void enqueue(struct queue *q, struct game_state state) 
+{
+    // append serialized_state to queue.data (use linked list's insert_at_tail)
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+    insert_at_tail(q, state);
 
-int number_of_moves(struct game_state start) { return 0; }
+}
+
+
+// dequeue function: Remove and return the deserialized game state at the head of the queue
+struct game_state dequeue(struct queue *q) 
+{
+    // if queue.data is empty:
+    //     return null // or an invalid game_state
+    // serialized_state = remove_from_head(queue.data)
+    // return deserialize(serialized_state)
+    
+    if (q -> data == NULL) return NULL;
+    return (struct game_state){0};
+}
+
+
+// number_of_moves function: Perform BFS to find the shortest path to solve the Tiles game
+int number_of_moves(struct game_state start) 
+{
+    // Create a queue and a set for visited states
+    queue = initialize queue
+    visited = initialize empty set
+
+    // Add the start state to the queue and mark it as visited
+    enqueue(queue, start)
+    mark serialize(start) as visited
+
+    while queue is not empty:
+        current_state = dequeue(queue)
+
+        // If the current state is the goal, return the number of steps
+        if is_goal_state(current_state):
+            return current_state.num_steps
+
+        // Generate all possible next states (up, down, left, right)
+        for each move in [move_up, move_down, move_left, move_right]:
+            next_state = apply move to current_state
+            serialized_next_state = serialize(next_state)
+
+            // If the next state has not been visited
+            if serialized_next_state is not in visited:
+                mark serialized_next_state as visited
+                increment next_state.num_steps by 1
+                enqueue(queue, next_state)
+            end if
+        end for
+    end while
+
+    return -1 // Return -1 if no solution is found
+
+    return 0;
+}
